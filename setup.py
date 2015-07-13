49951a9ea230deac3bf45201d74d3eb78a0bed14
Joe Brown <brwnjm@gmail.com>
2015-07-13 15:30:24 -0400

    version bump

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
+import os.path as op
 from setuptools import setup
 
+version_py = op.join(op.dirname(__file__), 'version.py')
+with open(version_py) as v:
+    version = v.read().strip().split("=")[-1].strip().strip('"')
 
 setup(
     name='blastdbget',
-    version='0.0.7',
+    version=version,
     url='http://github.com/brwnj/blastdbget',
     license='MIT',
     author='Joe Brown',
@@ -11,11 +15,9 @@ setup(
     description='Blast DB downloads via NCBI FTP.',
     long_description=__doc__,
     py_modules=['blastdbget'],
-    install_requires=[
-        'click>=2.0',
-    ],
+    install_requires=['sh'],
     entry_points='''
         [console_scripts]
-        blastdbget=blastdbget:download
+        blastdbget=blastdbget:main
     '''
 )
